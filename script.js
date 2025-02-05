3d74231eda2e6ce9d67730917e631b212b90b693
leanelys <leanelys.gonzalez@upr.edu>
2025-02-04 21:44:16 -0400

    Added basic sketchpad functionality
    User can press and hold their mouse to draw on the pixel grid.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const pixelGridContainer = document.getElementById("pixel-grid-container");
+let draw = false;
 let gridSize = 16;
 
 for (let i = 0; i < gridSize*gridSize; i++) {
     let square = document.createElement("div");
     square.classList.add("grid-square");
     pixelGridContainer.appendChild(square);
+    square.addEventListener("mouseover", () => {
+        if (draw) {
+            square.classList.remove("grid-square");
+            square.classList.add("colored-grid-square");
+        }
+    })
+    square.addEventListener("mousedown", () => {
+        draw = true;
+    })
+    square.addEventListener("mouseup", () => {
+        draw = false;
+    })
 }
