5a0b024cbfdeb3acb18a928cc11d483778712a62
leanelys <leanelys.gonzalez@upr.edu>
2025-02-08 15:54:17 -0400

    Change options to buttons
    Options used to be just text divs. They are now buttons with click event listeners attached to them. These event listeners currently have no functionality.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// create function that makes a new pixel grid (can be used by clearCanvas)
+
 const pixelGridContainer = document.getElementById("pixel-grid-container");
 let draw = false;
 let gridSize = 16;
@@ -23,3 +25,19 @@ for (let i = 0; i < gridSize*gridSize; i++) {
         draw = false;
     })
 }
+
+const drawButton = document.getElementById("draw-btn");
+drawButton.addEventListener("click", () =>{
+    
+})
+
+let erase = false;
+const eraseButton = document.getElementById("erase-btn");
+eraseButton.addEventListener("click", () => {
+
+})
+
+const clearCanvasButton = document.getElementById("clear-canvas-btn");
+clearCanvasButton.addEventListener("click", () => {
+
+})
