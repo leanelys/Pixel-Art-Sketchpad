90517f41a90712a7fb119f68efcd23273a84c2f7
leanelys <leanelys.gonzalez@upr.edu>
2025-02-08 16:28:37 -0400

    Refactor pixel grid creation
    Pixel grid is now created with a function that accepts a grid size instead of being a fixed value.

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
-// create function that makes a new pixel grid (can be used by clearCanvas)
+// ===============================================================================================
+// FUNCTIONS
+// ===============================================================================================
 
-const pixelGridContainer = document.getElementById("pixel-grid-container");
-let draw = false;
-let gridSize = 16;
+function createPixelGrid(gridSize) {
+    const pixelGridContainer = document.getElementById("pixel-grid-container");
 
-for (let i = 0; i < gridSize*gridSize; i++) {
-    let square = document.createElement("div");
-    square.classList.add("grid-square");
-    pixelGridContainer.appendChild(square);
-    square.addEventListener("mouseover", () => {
-        if (draw) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        let square = document.createElement("div");
+        square.classList.add("grid-square");
+        pixelGridContainer.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            if (draw) {
+                square.classList.remove("grid-square");
+                square.classList.add("colored-grid-square");
+            }
+        })
+        square.addEventListener("click", () => {
             square.classList.remove("grid-square");
             square.classList.add("colored-grid-square");
-        }
-    })
-    square.addEventListener("click", () => {
-        square.classList.remove("grid-square");
-        square.classList.add("colored-grid-square");
-    })
-    square.addEventListener("mousedown", () => {
-        draw = true;
-    })
-    square.addEventListener("mouseup", () => {
-        draw = false;
-    })
+        })
+        square.addEventListener("mousedown", () => {
+            draw = true;
+        })
+        square.addEventListener("mouseup", () => {
+            draw = false;
+        })
+    }
 }
 
+// ===============================================================================================
+// MAIN LOGIC
+// ===============================================================================================
+
+// Loads the default pixel grid (16x16)
+document.addEventListener("DOMContentLoaded", () => {
+    let draw = false;
+    createPixelGrid(16);
+})
+
+// Draw mode
 const drawButton = document.getElementById("draw-btn");
 drawButton.addEventListener("click", () =>{
     
 })
 
+// Erase mode
 let erase = false;
 const eraseButton = document.getElementById("erase-btn");
 eraseButton.addEventListener("click", () => {
 
 })
 
+// Clear/erase entire canvas
 const clearCanvasButton = document.getElementById("clear-canvas-btn");
 clearCanvasButton.addEventListener("click", () => {
 
